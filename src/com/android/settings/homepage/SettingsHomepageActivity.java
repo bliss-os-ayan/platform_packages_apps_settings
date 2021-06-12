e8ba5768446a4001411119e35a902ffc55bc9086
Ayan Mukherjee <mukherjeeayan725@gmail.com>
2021-06-12 03:39:05 -0400

    Remove duplicate stuff
    Change-Id: I3ce5d86365483ac9ff2e3cb2b0de56e4ea8b74f7

--- src/com/android/settings/homepage/SettingsHomepageActivity.java
+++ src/com/android/settings/homepage/SettingsHomepageActivity.java
@@ -169,15 +169,4 @@ public class SettingsHomepageActivity extends FragmentActivity {
         avatarView.setImageDrawable(getCircularUserIcon(getApplicationContext()));
     }
 
-    private boolean isHomepageSpacerEnabled() {
-        return true;
-    }
-
-    private static void setMargins (View v, int l, int t, int r, int b) {
-        if (v.getLayoutParams() instanceof ViewGroup.MarginLayoutParams) {
-            ViewGroup.MarginLayoutParams p = (ViewGroup.MarginLayoutParams) v.getLayoutParams();
-            p.setMargins(l, t, r, b);
-            v.requestLayout();
-        }
-    }
 }
